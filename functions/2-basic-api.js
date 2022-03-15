8a2ca6e99c58792ff435f6a0d49ddf0dbafe4bb8
Sheetal Raikar <sheetal_raikar@persistent.com>
2022-03-15 14:40:21 +0530

    testing deploy

---
+++ functions/2-basic-api.js
@@ -0,0 +1,8 @@
+//domain/.netlify/functions/2-basic-api
+const items = require('../assets/data');
+exports.handler = async (event,context) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(items),
+    }
+}
