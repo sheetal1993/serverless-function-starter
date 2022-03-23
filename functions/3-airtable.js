d70a130e49cf0ecf6dbedc73364ddc98c1aaa0f3
Sheetal Raikar <sheetal_raikar@persistent.com>
2022-03-23 13:11:32 +0530

    airtable

---
+++ functions/3-airtable.js
@@ -0,0 +1,27 @@
+const Airtable = require('airtable-node');
+ 
+const airtable = new Airtable({ apiKey: 'keye3G1Eg1tMipodC' })
+  .base('appSHcUYCSR4oCq8l')
+  .table('products')
+exports.handler = async (event,context) => {
+    try {
+        const {records} = airtable.list();
+        const products = records.map((product) => {
+            const {id} = product;
+            const {name,image,price} = product.fields;
+            const url = image[0].url;
+            return {id,name,url,price}
+        })
+        return {
+            statusCode: 200,
+            body: JSON.stringify(products),
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'server error',
+        } 
+    }
+    
+    
+}
